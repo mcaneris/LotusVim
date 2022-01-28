e23b76d295699b5491476562c6239ffbb1376dd8
Mete Can Eris <can.eris@feyn.dev>
2022-01-28 10:39:32 +0300

    Fix formatting

--- init.lua
+++ init.lua
@@ -1,37 +1,37 @@
 vim.api.nvim_exec([[
-if (empty($TMUX))
-  if (has("nvim"))
-    let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+  if (empty($TMUX))
+    if (has("nvim"))
+      let $NVIM_TUI_ENABLE_TRUE_COLOR=1
     endif
     if (has("termguicolors"))
       set termguicolors
-      endif
-      endif
-      ]], true)
+    endif
+  endif
+]], true)
 
-      require('plugins')
-      require('lotus')
-      require('globals')
-      require('keymappings')
-      require('settings')
+require('plugins')
+require('lotus')
+require('globals')
+require('keymappings')
+require('settings')
 
-      local plugins = {
-        'barbar',
-        'coq',
-        'dashboard-nvim',
-        'galaxyline',
-        'gitsigns',
-        'lsp-colors',
-        'lspsaga',
-        'nvim-lsp-installer',
-        'nvim-treesitter',
-        'telescope',
-        'trouble',
-        'which-key'
-      }
+local plugins = {
+  'barbar',
+  'coq',
+  'dashboard-nvim',
+  'galaxyline',
+  'gitsigns',
+  'lsp-colors',
+  'lspsaga',
+  'nvim-lsp-installer',
+  'nvim-treesitter',
+  'telescope',
+  'trouble',
+  'which-key'
+}
 
-      for _, plugin in pairs(plugins) do
-        require('config.plugins.' .. plugin)
-      end
+for _, plugin in pairs(plugins) do
+  require('config.plugins.' .. plugin)
+end
 
-      require'nvim-tree'.setup {}
+require'nvim-tree'.setup {}
